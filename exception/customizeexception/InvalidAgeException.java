a97db841f075bca69c8ef50620b8104d22a52979
PrashulShetty <prashulshetty307@gmail.com>
2019-12-21 01:29:28 +0530

    customize exception

---
+++ exception/customizeexception/InvalidAgeException.java
@@ -0,0 +1,10 @@
+
+  package com.exception.customizeexception;
+  
+  public class InvalidAgeException extends Exception {
+             InvalidAgeException(String s) { 
+            	 System.out.println(s);
+            	 }
+  }
+  
+ 
