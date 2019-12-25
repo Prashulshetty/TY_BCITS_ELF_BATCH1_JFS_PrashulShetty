ad6b6cbea30d4f3be7e898d60258a672b0837b80
Prashulshetty <59006865+Prashulshetty@users.noreply.github.com>
2019-12-26 01:30:12 +0530

    hackerrank

--- HackerRankProject/src/com/bcits/hackerranksolution/Student.java
+++ HackerRankProject/src/com/bcits/hackerranksolution/Student.java
@@ -1,36 +1,28 @@
-package com.bcits.hackerranksolution;
+package com.bcits.hackerrank.datastructure;
 
 public class Student {
-	int id;
-	String name;
-	double cgpa;
+	private int id;
+	private String name;
+	private double cgpa;
 	public Student(int id, String name, double cgpa) {
 		super();
 		this.id = id;
 		this.name = name;
-		this.cgpa =cgpa ;
+		this.cgpa = cgpa;
 	}
 	public int getId() {
 		return id;
 	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
+	public String getname() {
 		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
 	}
 	public double getCgpa() {
 		return cgpa;
 	}
-	public void setCgpa(double cgpa) {
-		this.cgpa = cgpa;
-	}
+	
 	@Override
 	public String toString() {
-		return "Student [id=" + id + ", name=" + name + ", cgpa=" + cgpa + "]";
+		return name + " "+ cgpa +" " + id ;
 	}
 	
 
