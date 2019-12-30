064d031d312babcb3237ee1bccd52afbfb0954fc
Prashul shetty <prashulshetty307@gmail.com>
2019-12-30 18:14:02 +0530

    updated

---
+++ jdbc/connection/src/com/bcits/jdbc/connection/ClassB.java
@@ -0,0 +1,11 @@
+package com.bcits.jdbc.connection;
+
+public class ClassB implements Connection1 {
+	public void print() {
+		System.out.println("bbbbb");
+	}
+	/*
+	 * should provide implementation for all
+	 * 10 abstract methods
+	 */
+}// End of class
