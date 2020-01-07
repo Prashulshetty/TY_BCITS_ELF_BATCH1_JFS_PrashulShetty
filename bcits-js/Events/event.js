31533687356930ae4ea98405a968f7b105c6f0c3
PrashulShetty <prashulshetty307@gmail.com>
2020-01-07 23:45:44 +0530

    event updated

--- bcits-js/Events/event.js
+++ bcits-js/Events/event.js
@@ -1,59 +1,64 @@
 
 
-function showalert() {
-    alert('hi welcome to cinema hall......')
+function showAlert() {
+    alert('Hi.....')
+}
+function showCreatedElement() {
+    var createdElement = document.createElement('h1') 
+    
+    createdElement.textContent='Hello..Welcome'
+    document.body.appendChild(createdElement)
+    createdElement.style.display='block'
+    
+}
+function showName() { 
+    var dispName = document.getElementById('disp')
+   
+    if (dispName.style.display ==='' || dispName.style.display==='none' ) {
+        
+        dispName.style.display='block'
+        var hideName = document.getElementById('Display')
+        hideName.textContent='click here to hide name'
+
+    } else {
+        dispName.style.display='none'
+        var hideName = document.getElementById('Display')
+        hideName.textContent='click here to show name'
+
+    }
 }
 
 
 
-function showelement() {
-    var element = document.createElement('h2')
-    element.textContent = 'hi. good evening..'
-    document.body.appendChild(element)
-}
-
-function displayelement() {
-  var displayeleement = document.getElementById('disp')
-
-  var myBtn =document.getElementById('myBtn')
-  if(displayelement.style.display===''||
-  displayelement.style.display==='none'){
-
- 
-  displayelement.style.display='block'
-  myBtn.textContent= 'click here to hide h1 element'
-  } else {
-      displayelement.style.display='none'
-      myBtn.textContent='click to show h1'
-  }
-
-}
 
 function changecolorblue() {
     var changeblue = document.getElementById('change')
-    changeblue.style.backgroundColor='lightgreen'
-    changeblue.style.fontSize='50px'
+    changeblue.style.backgroundColor='blue'
+    changeblue.style.fontSize='100px'
 
 }
 function changecolorwhite() {
     var changeW = document.getElementById('change')
     changeW.style.backgroundColor='white'
-    changeW.style.fontsize='90px'
+   changeW.style.fontSize='50px'
 
 }
-// function showData() {
-//     console.log('working!!');
+
+function showData() {
+    console.log('working!!');
     
-// }
-// function showMessage() {
-//     console.log('on key executed.......!!');
+}
+function showMessage() {
+    console.log('on key executed.......!!');
+}
     
-// }
-// function getinputvalue() {
-//   var inpElements=  document.getElementById('inp')
-//   console.log(inpElements.value); 
-//   var values=document.getElementById('hidetext')
-//   values.textContent=inpElements.value ;
-  
-// }
+
+function getinputvalue() {
+  var inpElements=  document.getElementById('inp')
+  console.log(inpElements.value); 
+
+  var values=document.getElementById('hidetext')
+  values.textContent=inpElements.value ;
+}  
+
 
