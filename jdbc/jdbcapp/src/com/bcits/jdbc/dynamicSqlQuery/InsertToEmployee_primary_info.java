6f6fba2a02ca9ef658d070d4f922fc5bb07e475b
PrashulShetty <prashulshetty307@gmail.com>
2020-01-10 00:12:04 +0530

    dynamicsqlquery

--- jdbc/jdbcapp/src/com/bcits/jdbc/dynamicSqlQuery/InsertToEmployee_primary_info.java
+++ jdbc/jdbcapp/src/com/bcits/jdbc/dynamicSqlQuery/InsertToEmployee_primary_info.java
@@ -1,12 +1,15 @@
+
 package com.bcits.jdbc.dynamicSqlQuery;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Properties;
 
-public class UpdateRecord {
+public class InsertToEmployee_primary_info {
 
 	public static void main(String[] args) {
 		Connection con = null;
@@ -21,24 +24,28 @@ public class UpdateRecord {
 			String dburl = "jdbc:mysql://localhost:3306/employee_management_info";
 			con = DriverManager.getConnection(dburl, "root", "root");
 
-			// Issue sql query                                                              
-			/*
-			 * String query = " update employee_primary_info set emp_name ='bhumika' " +
-			 * " where emp_id=?";
-			 * 
-			 */
-			String query = " update employee_primary_info set emp_name = ? where emp_id= ? ";
+			// Issue sql query
+			String query = " insert into employee_primary_info values(?,?,?,?,?,?,?,?,?,?,?) ";
 			pstmt = con.prepareStatement(query);
-			pstmt.setString(1, args[0]);
-			pstmt.setInt(2, Integer.parseInt(args[1]));
-
+			pstmt.setInt(1, Integer.parseInt(args[0]));
+			pstmt.setString(2, (args[1]));
+			pstmt.setLong(3, Long.parseLong(args[2]));
+			pstmt.setString(4, (args[3]));
+			pstmt.setDate(5,java.sql.Date.valueOf(args[4]));
+			pstmt.setDate(6,java.sql.Date.valueOf(args[5]));
+			pstmt.setString(7, (args[6]));
+			pstmt.setString(8, (args[7]));
+			pstmt.setDouble(9,Double.parseDouble(args[8]));
+			pstmt.setInt(10,Integer.parseInt(args[9]) );
+			pstmt.setInt(11,Integer.parseInt(args[10]) );
+			
 			rs = pstmt.executeUpdate();
 
 			// Process the result returned by sql query
-			if (rs != 0) {
-				System.out.println("profile updated");
+			if (rs !=0) {
+				System.out.println("record inserted");
 			} else {
-				System.err.println("Employee data is not found in DB");
+				System.err.println("Employee data already exist");
 			}
 
 		} catch (Exception e) {
