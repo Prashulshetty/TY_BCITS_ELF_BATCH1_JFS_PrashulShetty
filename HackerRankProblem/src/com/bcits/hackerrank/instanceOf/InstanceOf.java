c52c0903f682600d468282cc7abb91ce52893440
PrashulShetty <prashulshetty307@gmail.com>
2020-01-10 23:12:28 +0530

    Instance Keyword

---
+++ HackerRankProblem/src/com/bcits/hackerrank/instanceOf/InstanceOf.java
@@ -0,0 +1,51 @@
+package com.bcits.hackerrank.instanceOf;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+class Student {
+}
+
+class Rockstar {
+}
+
+class Hacker {
+}
+
+public class InstanceOf {
+
+	static String count(ArrayList mylist) {
+		int a = 0, b = 0, c = 0;
+		for (int i = 0; i < mylist.size(); i++) {
+			Object element = mylist.get(i);
+			if (element instanceof Student)
+				a++;
+			if (element instanceof Rockstar)
+				b++;
+			if (element instanceof Hacker)
+				c++;
+		}
+		String ret = Integer.toString(a) + " " + Integer.toString(b) + " " + Integer.toString(c);
+		return ret;
+	}
+
+	public static void main(String[] args) {
+		ArrayList mylist = new ArrayList();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number= ");
+		int t = sc.nextInt();
+		System.out.println("Enter the instances...");
+		for (int i = 0; i < t; i++) {
+			String s = sc.next();
+			if (s.equals("Student"))
+				mylist.add(new Student());
+			if (s.equals("Rockstar"))
+				mylist.add(new Rockstar());
+			if (s.equals("Hacker"))
+				mylist.add(new Hacker());
+		}
+		System.out.println(count(mylist));
+		sc.close();
+	}
+}
