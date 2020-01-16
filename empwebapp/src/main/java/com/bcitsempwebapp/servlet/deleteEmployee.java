02620b222ae89350311b14cd1767bb0ca159c25b
Prashul shetty <prashulshetty307@gmail.com>
2020-01-16 13:52:50 +0530

    update

--- empwebapp/src/main/java/com/bcitsempwebapp/servlet/deleteEmployee.java
+++ empwebapp/src/main/java/com/bcitsempwebapp/servlet/deleteEmployee.java
@@ -5,20 +5,18 @@ import java.io.PrintWriter;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax
-
-
-.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.bcits.empwebapp.beans.EmployeePrimary;
 
-@WebServlet("/findEmployee")
-public class SearchEmployeeServlet2 extends HttpServlet {
+@WebServlet("/deleteEmployee")
+public class deleteEmployee extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String empIdVal = req.getParameter("empId");
@@ -26,27 +24,28 @@ public class SearchEmployeeServlet2 extends HttpServlet {
 
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("emsPersistenceUnit");
 		EntityManager manager = entityManagerFactory.createEntityManager();
+
+		EntityTransaction transaction = manager.getTransaction();
+		transaction.begin();
 		EmployeePrimary employeePrimaryInfo = manager.find(EmployeePrimary.class, empId);
 
 		resp.setContentType("text/html");
 
 		PrintWriter out = resp.getWriter();
-		if (employeePrimaryInfo != null) {
+
+		if (employeePrimaryInfo == null) {
 			out.println("<html>");
 			out.println("<body>");
-			out.println("<h1 style='color:green'>Employee ID" + empId + "found -<h1>");
-			out.println("name =" + employeePrimaryInfo.getEmp_name());
-			out.println("<br> Designation = " + employeePrimaryInfo.getDesignation());
-			out.println("<br> salary = " + employeePrimaryInfo.getSalary());
+			out.println("<h1 style='color:red'>Employee details for empId " + empId + " not found -<h1>");
 			out.println("</body>");
 			out.println("</html>");
-
 		} else {
+			manager.remove(employeePrimaryInfo);
+			transaction.commit();
+
 			out.println("<html>");
 			out.println("<body>");
-			out.println("<h1 style='color:red'>Employee ID" + empId + " not found -<h1>");
-
-			
+			out.println("<h1 style='color:red'>Employee record for " + empId + " is deleted -<h1>");
 			out.println("</body>");
 			out.println("</html>");
 		}
