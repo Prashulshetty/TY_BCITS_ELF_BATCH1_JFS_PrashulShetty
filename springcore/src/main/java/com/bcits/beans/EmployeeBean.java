301c795623c18d6f22cd3d7e66ff7661c3612338
Prashul shetty <prashulshetty307@gmail.com>
2020-01-23 18:30:20 +0530

    spring core

--- springcore/src/main/java/com/bcits/beans/EmployeeBean.java
+++ springcore/src/main/java/com/bcits/beans/EmployeeBean.java
@@ -1,11 +1,27 @@
 package com.bcits.beans;
 
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
 @Data
+//@AllArgsConstructor
 public class EmployeeBean {
+
 private String name;
-private int age;
+private int age; 
 private DepartmentBean departmentBean;
+
+private EmployeeBean( ) {
+	
+}
+
+public EmployeeBean(String name, int age, DepartmentBean departmentBean) {
+	super();
+	this.name = name;
+	this.age = age;
+	this.departmentBean = departmentBean;
+}
+
 	
 
 }
